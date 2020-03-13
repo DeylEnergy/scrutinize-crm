9a974520544dc14fedaab0e076ddf334236960bd
Deyl <soulvisman@gmail.com>
2020-03-13 06:37:45 +0600

    Setup the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './index.css'
+import AppStyles from './AppStyles.styles.tw'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppStyles>
+      <h1>Greetings Earthling</h1>
+      <p>
+        Welcome to your Create-React-App / TailwindCSS / Styled Components
+        template
+      </p>
+      <h2>Resources / Documentation</h2>
+      <ul>
+        <li>
+          <a href="https://reactjs.org/docs/create-a-new-react-app.html">
+            ReactJS
+          </a>
+        </li>
+        <li>
+          <a href="https://tailwindcss.com/">TailwindCSS</a>
+        </li>
+        <li>
+          <a href="https://styled-components.com/">Styled Components</a>
+        </li>
+      </ul>
+    </AppStyles>
+  )
 }
 
-export default App;
+export default App
