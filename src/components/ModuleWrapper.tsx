7b534144ea85c4a6ca0c7fea74515d44e917d29f
Deyl <soulvisman@gmail.com>
2020-08-17 08:24:07 +0600

    Add <ModuleWrapper />

---
+++ src/components/ModuleWrapper.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const ModuleWrapper = styled.div`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  position: relative;
+`
+
+export default ModuleWrapper
