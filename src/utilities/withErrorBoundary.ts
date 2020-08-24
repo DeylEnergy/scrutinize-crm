e678e3f4d691cfbd8c736fb8ec27509740e1056c
Deyl <soulvisman@gmail.com>
2020-08-25 05:08:38 +0600

    Make withErrorBoundary more compatible

--- src/utilities/withErrorBoundary.ts
+++
@@ -1,8 +0,0 @@
-import {withErrorBoundary as withEB} from 'react-error-boundary'
-import ErrorBoundaryFallback from '../components/ErrorBoundaryFallback'
-
-export default function withErrorBoundary(Component: any) {
-  return withEB(Component, {
-    FallbackComponent: ErrorBoundaryFallback,
-  })
-}
