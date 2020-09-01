52c0a5cbd1c82860e17f8b17c4afb46018e91d37
Deyl <soulvisman@gmail.com>
2020-09-01 11:40:53 +0600

    Add layouts: <PageWrapper /> && <ControlWrapper />

---
+++ src/layouts/PageWrapper.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const PageWrapper = styled.div`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  position: relative;
+`
+
+export default PageWrapper
