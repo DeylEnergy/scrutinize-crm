2d47be808ea65e9aea4500948765252986fdb956
Deyl <soulvisman@gmail.com>
2020-09-14 06:03:44 +0600

    Modify acquisitions/filters as to previous changes

--- src/worker/acquisitions/filters.ts
+++ src/worker/acquisitions/filters.ts
@@ -1,8 +1,8 @@
-const filters = {
-  active: (x: any) => !x.isFrozen,
-  haveToBuy: (x: any) => !x.isDone && !x.isFrozen,
-  bought: (x: any) => x.isDone,
-  frozen: (x: any) => x.isFrozen,
+export default function filters() {
+  return {
+    active: (x: any) => !x.isFrozen,
+    haveToBuy: (x: any) => !x.isDone && !x.isFrozen,
+    bought: (x: any) => x.isDone,
+    frozen: (x: any) => x.isFrozen,
+  }
 }
-
-export default filters
