7dce15178724829a112545d7c8f73514741e394a
Deyl <soulvisman@gmail.com>
2020-09-22 07:37:27 +0600

    Delete useGroupPermissions && groupPermissionsContext

--- src/contexts/groupPermissionsContext.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const GroupPermissionsContext = React.createContext<any>({})
-
-export default GroupPermissionsContext
--- src/utilities/useGroupPermissions.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import GroupPermissionsContext from '../contexts/groupPermissionsContext'
-
-export default function useGroupPermissions() {
-  const ctx = React.useContext(GroupPermissionsContext)
-
-  return ctx
-}
