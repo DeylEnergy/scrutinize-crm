d0bc093bfcae1e2515936713edc424dc8c8f1391
Deyl <soulvisman@gmail.com>
2020-09-22 07:33:32 +0600

    Add rights const

---
+++ src/constants/rights.ts
@@ -0,0 +1,14 @@
+export default {
+  CAN_SEE_PRODUCTS: 'canSeeProducts',
+  CAN_EDIT_PRODUCTS: 'canEditProducts',
+  CAN_SEE_TO_BUY_LIST: 'canSeeToBuyList',
+  CAN_EDIT_ITEMS_IN_TO_BUY_LIST: 'canEditItemsInToBuyItems',
+  CAN_ADD_ITEM_TO_BUY_LIST: 'canAddItemToBuyList',
+  CAN_PRINT_TO_BUY_LIST: 'canPrintToBuyList',
+  CAN_COMPLETE_TO_BUY_LIST: 'canCompleteToBuyList',
+  CAN_SEE_ACQUISITIONS: 'canSeeAcquisitions',
+  CAN_SEE_SALES: 'canSeeSales',
+  CAN_RETURN_SALES_ITEMS: 'canReturnSalesItems',
+  CAN_SEE_USERS: 'canSeeUsers',
+  CAN_SEE_USERS_GROUP: 'canSeeUsersGroup',
+}
