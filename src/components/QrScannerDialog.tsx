420e0c8b89b69f00702557fd72ba0bd2e4333031
Deyl <soulvisman@gmail.com>
2020-10-09 13:07:58 +0600

    Add <QRScannerDialog />

---
+++ src/components/QrScannerDialog.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import {Button, CameraIcon, Pane, Position} from 'evergreen-ui'
+import styled from 'styled-components'
+import QrScanner from './QRScanner'
+import Popover from './Popover'
+import {useScannerListener} from '../utilities'
+
+const StyledSpan = styled.span<{isGlobal: boolean}>`
+  ${({isGlobal}) => {
+    return (
+      isGlobal &&
+      `
+      position: absolute;
+      right: 32px;
+      bottom: 32px;
+      opacity: 0.7;
+      &:hover {
+        opacity: 1;
+      }
+      `
+    )
+  }}
+`
+
+function QRScannerPopover({
+  cameraSize = 400,
+  isGlobal = false,
+  isShown,
+  setIsShown,
+}: any) {
+  const [, setScannerListener] = useScannerListener()
+
+  const handleResult = React.useCallback(
+    (scanResult: string) => {
+      setScannerListener({value: scanResult})
+    },
+    [setScannerListener],
+  )
+
+  const cameraWidth = cameraSize
+  const cameraHeight = Math.floor(cameraWidth / 1.333)
+
+  return (
+    <Popover
+      isShown={isShown}
+      content={
+        <Pane padding={8} width={cameraWidth + 16} height={cameraHeight + 16}>
+          <QrScanner
+            onInactive={() => setIsShown(false)}
+            onResult={handleResult}
+            cameraWidth={cameraWidth}
+            cameraHeight={cameraHeight}
+          />
+        </Pane>
+      }
+      position={Position.TOP_RIGHT}
+      onBodyClick={() => setIsShown(false)}
+    >
+      <StyledSpan isGlobal={isGlobal}>
+        <Button onClick={() => setIsShown(true)} iconBefore={CameraIcon}>
+          Open Scanner
+        </Button>
+      </StyledSpan>
+    </Popover>
+  )
+}
+
+export default QRScannerPopover
