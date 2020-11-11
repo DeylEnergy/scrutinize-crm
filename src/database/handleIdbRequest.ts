cad369d03cfc2df61d5a9e97e10391f2cf2f8142
Deyl <soulvisman@gmail.com>
2020-11-11 13:16:36 +0600

    Add rejection control for handleIdbRequest

--- src/database/handleIdbRequest.ts
+++ src/database/handleIdbRequest.ts
@@ -1,5 +1,6 @@
 export default function handleIdbRequest(req: IDBRequest) {
-  return new Promise(
-    resolve => (req.onsuccess = (res: any) => resolve(res.target.result)),
-  )
+  return new Promise((resolve, reject) => {
+    req.onsuccess = (res: any) => resolve(res.target.result)
+    req.onerror = (e: any) => reject(e.target.error)
+  })
 }
