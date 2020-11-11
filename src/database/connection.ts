4edc87d0569588dbc7d3927ed6bb61f5f7a55d80
Deyl <soulvisman@gmail.com>
2020-11-11 13:22:07 +0600

    Check existence of object stores instead of version approach

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -3,22 +3,26 @@ const currentVersion = 24
 
 const db: {current: null | IDBDatabase} = {current: null}
 const dbReq: IDBOpenDBRequest = indexedDB.open('mydbx', currentVersion)
-
 dbReq.onupgradeneeded = () => {
   db.current = dbReq.result
 
   const keyPath = {keyPath: 'id'}
-  // @ts-ignore
-  if (currentVersion === 1) {
+
+  const objectStores = db.current.objectStoreNames
+  if (!objectStores.contains(SN.PRODUCTS)) {
     db.current
       .createObjectStore(SN.PRODUCTS, keyPath)
       .createIndex(IN.NAME_MODEL, IN.NAME_MODEL)
+  }
 
+  if (!objectStores.contains(SN.SALES)) {
     const sales = db.current.createObjectStore(SN.SALES, keyPath)
     sales.createIndex(IN.DATETIME, IN.DATETIME)
     sales.createIndex(IN.__CART_ID__, IN.__CART_ID__)
     sales.createIndex(IN.CART_PARTICIPANTS, IN.CART_PARTICIPANTS)
+  }
 
+  if (!objectStores.contains(SN.ACQUISITIONS)) {
     const acquisitionsStore = db.current.createObjectStore(
       SN.ACQUISITIONS,
       keyPath,
@@ -28,22 +32,51 @@ dbReq.onupgradeneeded = () => {
       IN.NEEDED_SINCE_DATETIME,
       IN.NEEDED_SINCE_DATETIME,
     )
+  }
 
+  if (!objectStores.contains(SN.CUSTOMERS)) {
     db.current.createObjectStore(SN.CUSTOMERS, keyPath)
+  }
+
+  if (!objectStores.contains(SN.USERS)) {
     db.current
       .createObjectStore(SN.USERS, keyPath)
       .createIndex(IN.NAME, IN.NAME)
+  }
+
+  if (!objectStores.contains(SN.USERS_STATS)) {
     db.current.createObjectStore(SN.USERS_STATS, {keyPath: 'userIdPeriod'})
+  }
+
+  if (!objectStores.contains(SN.GROUPS)) {
     db.current.createObjectStore(SN.GROUPS, keyPath)
+  }
+
+  if (!objectStores.contains(SN.SUPPLIERS)) {
     db.current.createObjectStore(SN.SUPPLIERS, keyPath)
+  }
+
+  if (!objectStores.contains(SN.STATS)) {
     db.current.createObjectStore(SN.STATS, {keyPath: 'period'})
+  }
+
+  if (!objectStores.contains(SN.BUDGET)) {
     db.current.createObjectStore(SN.BUDGET, keyPath)
+  }
+
+  if (!objectStores.contains(SN.CASHBOX_HISTORY)) {
     db.current
       .createObjectStore(SN.CASHBOX_HISTORY, keyPath)
       .createIndex(IN.DATETIME, IN.DATETIME)
+  }
+
+  if (!objectStores.contains(SN.EVENTS)) {
     db.current
       .createObjectStore(SN.EVENTS, keyPath)
       .createIndex(IN.EVENT_DATETIME, IN.EVENT_DATETIME)
+  }
+
+  if (!objectStores.contains(SN.ERRORS)) {
     db.current
       .createObjectStore(SN.ERRORS, keyPath)
       .createIndex(IN.ERROR_DATETIME, IN.ERROR_DATETIME)
