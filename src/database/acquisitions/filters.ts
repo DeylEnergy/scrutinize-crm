40f72d84352c6650c4977353eb8857816c3ad156
Deyl <soulvisman@gmail.com>
2020-11-20 07:33:11 +0600

    Restore accidently deleted filter methods of acquisitons

--- src/database/acquisitions/filters.ts
+++ src/database/acquisitions/filters.ts
@@ -1,8 +1,11 @@
 export default function filters({productId}: any) {
-  debugger
   return {
     productId: ({_productId}: any) => {
       return _productId === productId
     },
+    active: (x: any) => !x.isFrozen,
+    haveToBuy: (x: any) => !x.isDone && !x.isFrozen,
+    bought: (x: any) => x.isDone,
+    frozen: (x: any) => x.isFrozen,
   }
 }
