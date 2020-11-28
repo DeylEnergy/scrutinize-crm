6cf8e6808b742f3de9941400862a029fffacc2d5
Deyl <soulvisman@gmail.com>
2020-11-28 10:01:58 +0600

    Reverse period type for fixing sorting

--- src/utilities/getPeriodOfDate.ts
+++ src/utilities/getPeriodOfDate.ts
@@ -1,7 +1,7 @@
 export default function getPeriodOfDate(date: Date) {
   const currentMonth = String(date.getMonth() + 1).padStart(2, '0')
   const currentYear = date.getFullYear()
-  const currentMonthYear = `${currentMonth}/${currentYear}`
+  const currentYearMonth = `${currentYear}/${currentMonth}`
 
-  return currentMonthYear
+  return currentYearMonth
 }
