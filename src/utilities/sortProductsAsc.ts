52edd9f8880bcee521d370e9aa195c7704c9ca0d
Deyl <soulvisman@gmail.com>
2020-12-02 18:40:12 +0600

    Add sortProductsAsc function

---
+++ src/utilities/sortProductsAsc.ts
@@ -0,0 +1,22 @@
+export default function sortProductsAsc(rows: any[]) {
+  return rows.sort((a: any, b: any) => {
+    const nameFirst = a.name || a._product.nameModel[0]
+    const modelFirst = a.model || a._product.nameModel[1]
+
+    const nameSecond = b.name || b._product.nameModel[0]
+    const modelSecond = b.model || b._product.nameModel[1]
+
+    const first = [nameFirst.toLowerCase(), modelFirst.toLowerCase()]
+    const second = [nameSecond.toLowerCase(), modelSecond.toLowerCase()]
+
+    if (first < second) {
+      return -1
+    }
+
+    if (first > second) {
+      return 1
+    }
+
+    return 0
+  })
+}
