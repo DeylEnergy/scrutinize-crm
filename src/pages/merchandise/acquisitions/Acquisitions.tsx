e72537ef9812352937e803394ad5d63cfd41b236
Deyl <soulvisman@gmail.com>
2020-12-11 12:43:39 +0600

    Can edit inStockCount of acquisitions

--- src/pages/merchandise/acquisitions/Acquisitions.tsx
+++ src/pages/merchandise/acquisitions/Acquisitions.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import {Menu, IconButton, MoreIcon, EditIcon, Position} from 'evergreen-ui'
 import Table from '../../../components/Table'
+import UpdateAcquisitionInStockCount from './UpdateAcquisitionInStockCount'
+import Popover from '../../../components/Popover'
 import {
   useLocale,
   useDatabase,
@@ -7,6 +10,7 @@ import {
   withErrorBoundary,
 } from '../../../utilities'
 import {STORE_NAME as SN, INDEX_NAME as IN} from '../../../constants'
+import {UPDATE_ACQUISITION_IN_STOCK_COUNT} from '../../../constants/events'
 
 const FETCH_ITEM_LIMIT = 20
 
@@ -14,6 +18,11 @@ const LOADED_ITEMS_DEFAULT = {
   hasNextPage: true,
   items: [],
   lastKey: null,
+}
+
+const SIDE_SHEET_DEFAULT = {
+  value: null,
+  isShown: false,
 }
 
 function Acquisitions() {
@@ -24,14 +33,33 @@ function Acquisitions() {
 
   const makeCancellablePromise = useCancellablePromise()
 
-  const [loadedItems, setLoadedItems] = React.useReducer(
+  const [sideSheet, setSideSheet] = React.useReducer(
     // @ts-ignore
     (s, v) => ({...s, ...v}),
+    SIDE_SHEET_DEFAULT,
+  )
+
+  const itemsRef = React.useRef<any>(null)
+
+  const [loadedItems, setLoadedItems] = React.useReducer(
+    // @ts-ignore
+    (s, v) => {
+      const updated = {...s, ...v}
+      itemsRef.current = updated.items
+      return updated
+    },
     LOADED_ITEMS_DEFAULT,
   )
 
   const serializeItem = React.useCallback(
     (item: any) => {
+      const editSideSheet = () => {
+        setSideSheet({
+          value: {id: item.id, inStockCount: item.inStockCount},
+          isShown: true,
+        })
+      }
+
       const nameModel =
         item?._product?.nameModel || item?._legacyProductNameModel
 
@@ -55,9 +83,25 @@ function Acquisitions() {
           item?._productId?.split('-')[0],
           aqIdCell,
         ],
+        optionsMenu: (
+          <Popover
+            content={
+              <Menu>
+                <Menu.Group>
+                  <Menu.Item onSelect={editSideSheet} icon={EditIcon}>
+                    {PAGE_CONST.TABLE.OPTIONS.EDIT_IN_STOCK_COUNT}
+                  </Menu.Item>
+                </Menu.Group>
+              </Menu>
+            }
+            position={Position.BOTTOM_RIGHT}
+          >
+            <IconButton icon={MoreIcon} height={24} appearance="minimal" />
+          </Popover>
+        ),
       }
     },
-    [STRING_FORMAT],
+    [STRING_FORMAT, PAGE_CONST],
   )
 
   const isItemLoaded = React.useCallback(
@@ -94,6 +138,35 @@ function Acquisitions() {
     serializeItem,
   ])
 
+  const handleAcquisitionInStockCountUpdate = React.useCallback(
+    updatedAcquisition => {
+      db.sendEvent({
+        type: UPDATE_ACQUISITION_IN_STOCK_COUNT,
+        payload: updatedAcquisition,
+        consumer: 'client',
+      }).then((result: any) => {
+        if (!result) {
+          return
+        }
+
+        const {acquisition} = result
+
+        const items = itemsRef.current
+
+        const foundIndex = items.findIndex((x: any) => x.id === acquisition.id)
+        items[foundIndex] = serializeItem(acquisition)
+
+        setLoadedItems({items: [...items]})
+        setTimeout(() => setSideSheet({isShown: false}))
+      })
+    },
+    [db, serializeItem],
+  )
+
+  const handleSlideSheetCloseComplete = React.useCallback(() => {
+    setSideSheet(SIDE_SHEET_DEFAULT)
+  }, [])
+
   const columns = React.useMemo(() => {
     const {COLUMNS} = PAGE_CONST.TABLE
     return [
@@ -110,17 +183,29 @@ function Acquisitions() {
         label: COLUMNS.ACQUISITION_ID.TITLE,
         width: COLUMNS.ACQUISITION_ID.WIDTH,
       },
+      {label: 'OPTIONS', width: 50},
     ]
   }, [PAGE_CONST])
 
   return (
-    <Table
-      columns={columns}
-      rows={loadedItems.items}
-      hasNextPage={loadedItems.hasNextPage}
-      isItemLoaded={isItemLoaded}
-      loadMoreItems={loadMoreItems}
-    />
+    <>
+      <Table
+        columns={columns}
+        rows={loadedItems.items}
+        hasNextPage={loadedItems.hasNextPage}
+        isItemLoaded={isItemLoaded}
+        loadMoreItems={loadMoreItems}
+      />
+      {sideSheet.value && (
+        <UpdateAcquisitionInStockCount
+          sideSheet={sideSheet}
+          handleAcquisitionInStockCountUpdate={
+            handleAcquisitionInStockCountUpdate
+          }
+          onCloseComplete={handleSlideSheetCloseComplete}
+        />
+      )}
+    </>
   )
 }
 
